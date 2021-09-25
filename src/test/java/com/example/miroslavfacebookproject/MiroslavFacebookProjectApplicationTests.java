b1eddf1eba7d7a70aef2fb264318412bdf6ebfe9
Miroslav Emilov Pavlov <pavlov.miroslav.92@gmail.com>
2021-09-25 22:20:54 +0300

    project creation

---
+++ src/test/java/com/example/miroslavfacebookproject/MiroslavFacebookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miroslavfacebookproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiroslavFacebookProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
