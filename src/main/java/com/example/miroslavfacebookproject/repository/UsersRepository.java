f68de3eecb38d3e810049b24c059cac882de7505
Miroslav Emilov Pavlov <pavlov.miroslav.92@gmail.com>
2021-09-28 15:46:40 +0300

    add repositorys

---
+++ src/main/java/com/example/miroslavfacebookproject/repository/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.example.miroslavfacebookproject.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository {
+}
