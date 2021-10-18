1674def67b283a45ff73f103fb62d834994edc77
Miroslav <pavlov.miroslav.92@gmail.com>
2021-10-18 17:25:19 +0300

    add post entity

---
+++ src/main/java/com/example/miroslavfacebookproject/entity/Post.java
@@ -0,0 +1,66 @@
+package com.example.miroslavfacebookproject.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "status")
+    private Integer status; //1- only for user, 2- for friends and user, 3- for all
+
+    public Post(){}
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private Image image;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    private User poster;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public User getPoster() {
+        return poster;
+    }
+
+    public void setPoster(User poster) {
+        this.poster = poster;
+    }
+}
