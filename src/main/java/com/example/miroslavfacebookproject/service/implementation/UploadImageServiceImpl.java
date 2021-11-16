990c3ea6e4c99f7b6188bb370944cb1f8add8237
Miroslav <pavlov.miroslav.92@gmail.com>
2021-11-16 20:58:20 +0200

    take token

--- src/main/java/com/example/miroslavfacebookproject/service/implementation/UploadImageServiceImpl.java
+++ src/main/java/com/example/miroslavfacebookproject/service/implementation/UploadImageServiceImpl.java
@@ -3,7 +3,10 @@ package com.example.miroslavfacebookproject.service.implementation;
 import com.example.miroslavfacebookproject.controller.BaseController;
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.MetadataConfig;
 import com.google.cloud.storage.*;
+import io.grpc.Metadata;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
@@ -14,6 +17,8 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -21,23 +26,33 @@ public class UploadImageServiceImpl extends BaseController {
 
     private static final String DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/facebookproject-3ac7a.appspot.com/o/%s?alt=image/jpeg";
     private static String TEMP_URL = "http://localhost:8080/profile";
+    private static final String KEY = "C:\\Users\\PC\\IdeaProjects\\MiroslavFacebookProject\\src\\main\\resources\\serviceAccountKey.json";
+
+    public String downloadUrl;
+    public String image;
 
     private String uploadFile(File file, String fileName) throws IOException {
         BlobId blobId = BlobId.of("facebookproject-3ac7a.appspot.com", fileName);
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("image/jpeg").build();
-        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("C:\\Users\\PC\\IdeaProjects\\MiroslavFacebookProject\\src\\main\\resources\\serviceAccountKey.json"));
+        Map<String, String> map = new HashMap<>();
+        map.put("firebaseStorageDownloadTokens", fileName);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+                .setContentType("image/jpeg")
+                .setMetadata(map)
+                .build();
+        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream(KEY));
         Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
         storage.create(blobInfo, Files.readAllBytes(file.toPath()));
+
         return String.format(DOWNLOAD_URL, URLEncoder.encode(fileName, StandardCharsets.UTF_8));
     }
 
     private File convertToFile(MultipartFile multipartFile, String fileName) throws IllegalStateException, IOException {
         File tempFile = new File(fileName);
-        try (
-                FileOutputStream fos = new FileOutputStream(tempFile)) {
+        try (FileOutputStream fos = new FileOutputStream(tempFile)) {
             fos.write(multipartFile.getBytes());
             fos.close();
         }
+        image = fileName;
         return tempFile;
     }
 
@@ -61,16 +76,14 @@ public class UploadImageServiceImpl extends BaseController {
         return fileName.substring(fileName.lastIndexOf("."));
     }
 
-    public Object download(String fileName) throws IOException {
-        String destFileName = UUID.randomUUID().toString().concat(this.getExtension(fileName));     // to set random strinh for destination file name
-        String destFilePath = "D:\\New folder\\" + destFileName;                                    // to set destination file path
 
-        ////////////////////////////////   Download  ////////////////////////////////////////////////////////////////////////
-        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("C:\\Users\\PC\\IdeaProjects\\MiroslavFacebookProject\\src\\main\\resources\\serviceAccountKey.json"));
+
+    public void takeMetaData(String fileName) throws IOException, InterruptedException {
+
+        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream(KEY));
         Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
         Blob blob = storage.get(BlobId.of("facebookproject-3ac7a.appspot.com", fileName));
-        blob.downloadTo(Paths.get(destFilePath));
-        return send(TEMP_URL);
-    }
 
+        System.out.println(blob.toString());
+    }
 }
