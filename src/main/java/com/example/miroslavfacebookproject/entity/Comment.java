da532363f3f77e25ea96404de6e7a2e827fe8a8d
Miroslav <pavlov.miroslav.92@gmail.com>
2021-12-21 16:34:41 +0200

    add comments table

--- src/main/java/com/example/miroslavfacebookproject/entity/Comment.java
+++ src/main/java/com/example/miroslavfacebookproject/entity/Comment.java
@@ -1,4 +1,32 @@
 package com.example.miroslavfacebookproject.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "text")
+    private String commentText;
+
+    public Comment(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
 }
