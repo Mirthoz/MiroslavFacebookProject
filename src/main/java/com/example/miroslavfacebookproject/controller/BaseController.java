00f8082b05a880fea4c955e597f498c702d8ae57
Miroslav <pavlov.miroslav.92@gmail.com>
2021-12-22 14:12:55 +0200

    add comment form

--- src/main/java/com/example/miroslavfacebookproject/controller/BaseController.java
+++ src/main/java/com/example/miroslavfacebookproject/controller/BaseController.java
@@ -1,7 +1,9 @@
 package com.example.miroslavfacebookproject.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class BaseController {
 
     protected ModelAndView send(String viewName) {
--- src/main/java/com/example/miroslavfacebookproject/controller/CommentsController.java
+++ src/main/java/com/example/miroslavfacebookproject/controller/CommentsController.java
@@ -1,4 +1,18 @@
 package com.example.miroslavfacebookproject.controller;
 
-public class CommentsController extends BaseController{
+import com.example.miroslavfacebookproject.entity.User;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CommentsController extends BaseController {
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/add_comment")
+    public ModelAndView addComment(@AuthenticationPrincipal User currentUser){
+        System.out.println("Comment test " + currentUser.getUsername() + " " + currentUser.getSurname());
+        return redirect("profile");
+    }
 }
