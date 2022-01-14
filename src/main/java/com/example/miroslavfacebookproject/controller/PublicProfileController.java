58dbd7047df3b1066bb3b6b4570cbce2a4424e05
Miroslav <pavlov.miroslav.92@gmail.com>
2022-01-14 20:33:55 +0200

    add public profile functionality

--- src/main/java/com/example/miroslavfacebookproject/controller/PublicProfileController.java
+++ src/main/java/com/example/miroslavfacebookproject/controller/PublicProfileController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -31,7 +30,6 @@ public class PublicProfileController extends BaseController{
     public ModelAndView searchUserProfile(@ModelAttribute("searchProfile") SearchUserDTO searchUserDTO, Model model) {
         User user = userRepository.findUserById(searchUserDTO.getUserId());
         likeService.checkingLikes(user);
-        profileService.sendProfileData(user, model);
-        return send("public_profile");
+        return profileService.sendProfileData(user, model);
     }
 }
